5a2da560a935c8adbc848036f29e7eede8c9dc3c
pushkyn <alex.pushkyn@gmail.com>
2017-10-30 01:24:25 +0300

    add test for license year

--- test/test.py
+++ test/test.py
@@ -139,5 +139,18 @@ class TestSendGridEmail(unittest.TestCase):
                 )
             self.assertEqual(202, res[0])
 
+class TestLicenseYear(unittest.TestCase):
+    def setUp(self):
+        self.license_file = 'LICENSE.txt'
+
+    def test_license_year(self):
+        copyright_line = ''
+        with open(self.license_file, 'r') as f:
+            for line in f:
+                if line.startswith('Copyright'):
+                    copyright_line = line.strip()
+                    break
+        self.assertEqual('Copyright (c) 2016-%s SendGrid, Inc.' % datetime.datetime.now().year, copyright_line)
+
 if __name__ == '__main__':
     unittest.main()
